c76c83b74d0f5d0ac5ff3577e3efd5fee925a91c
Versity1 <vgsicthub@gmail.com>
2023-09-05 14:14:01 +0100

    static files upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
+
+@app.route("/table")
+def table():
+    return render_template('table.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
